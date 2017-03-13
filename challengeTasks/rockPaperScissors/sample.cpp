2408ddefbd0744b3ebcd459ad8ad6eb768ffef80
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-13 10:25:36 +0900

    Add error message

--- challengeTasks/rockPaperScissors/sample.cpp
+++ challengeTasks/rockPaperScissors/sample.cpp
@@ -28,8 +28,10 @@ int main(int argc, char** argv)
     std::cin >> your_input; // 入力を受け取る
 
     auto it = valid_input_map.find(your_input); // valid_input_mapから入力された手を探す
-    if (it == valid_input_map.end()) // not found
+    if (it == valid_input_map.end()) { // not found
+      std::cout << "Your input is false!" << std::endl;
       continue; // back to "Input your hand!"
+    }
 
     auto user_hand = it->second; // get your input.
 
