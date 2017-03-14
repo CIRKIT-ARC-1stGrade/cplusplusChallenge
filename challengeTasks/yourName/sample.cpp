42ecd1edbcec1431beb4fc108ba0890e5a6eed7a
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-15 02:48:36 +0900

    Remove unnecessary space

--- challengeTasks/yourName/sample.cpp
+++ challengeTasks/yourName/sample.cpp
@@ -7,7 +7,8 @@ int main(int argc, char** argv)
   std::string myname {"DOI YUSUKE"};
 
   std::cout << myname << std::endl; // display before sorting
-  
+
+  myname.erase(myname.find(' '), 1); // erase space on name
   std::sort(myname.begin(), myname.end()); // sort by number of a element.
 
   std::cout << myname << std::endl; // display after sorting
