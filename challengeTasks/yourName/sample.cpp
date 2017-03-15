3c9c4a348356c2f78f791917d9104b8a5df71aac
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-15 14:19:27 +0900

    Update yourName

--- challengeTasks/yourName/sample.cpp
+++ challengeTasks/yourName/sample.cpp
@@ -6,11 +6,11 @@ int main(int argc, char** argv)
 {
   std::string myname {"DOI YUSUKE"};
 
-  std::cout << myname << std::endl; // display before sorting
+  std::cout << "before:\n" << myname << std::endl; // display before sorting
 
   myname.erase(std::remove(myname.begin(), myname.end(), ' ')); // erase space on name
   std::sort(myname.begin(), myname.end()); // sort by number of a element.
 
-  std::cout << myname << std::endl; // display after sorting
+  std::cout << "after:\n" << myname << std::endl; // display after sorting
   return 0;
 }
