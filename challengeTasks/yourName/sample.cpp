37e828ac1d048ce89941fd7d3c867e2e67fd20ae
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-15 12:45:58 +0900

    Update erase method for space

--- challengeTasks/yourName/sample.cpp
+++ challengeTasks/yourName/sample.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 
   std::cout << myname << std::endl; // display before sorting
 
-  myname.erase(myname.find(' '), 1); // erase space on name
+  myname.erase(std::remove(myname.begin(), myname.end(), ' ')); // erase space on name
   std::sort(myname.begin(), myname.end()); // sort by number of a element.
 
   std::cout << myname << std::endl; // display after sorting
