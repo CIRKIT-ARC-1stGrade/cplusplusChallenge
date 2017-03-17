8fbff6d642c8a1fa009221d3ffe43c495aefa2ee
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-17 18:20:48 +0900

    Update for readable

--- challengeTasks/dictionary/sample.cpp
+++ challengeTasks/dictionary/sample.cpp
@@ -1,27 +1,21 @@
 #include <algorithm>
-#include <array>
-#include <cstddef>
 #include <iostream>
+#include <iterator>
 #include <random>
 #include <string>
 #include <utility>
-
-template<typename... Args>
-std::array<std::string, sizeof...(Args)> make_str_array(Args&&... args)
-{
-  return {std::forward<Args>(args)...};
-}
+#include <vector>
 
 int main(int argc, char** argv)
 {
-  auto names {make_str_array("apple", "apricot", "cherry", "kiwi fruit", "kumquat", "persimmon", "pomegranate", "strawberry")}; 
+  std::vector<std::string> names {"apple", "apricot", "cherry", "kiwi fruit", "kumquat", "persimmon", "pomegranate", "strawberry"};
   // above is correct order.
 
   std::shuffle(names.begin(), names.end(), std::default_random_engine{std::random_device{}()});
 
   std::cout << "before:\n";
-  std::for_each(names.begin(), names.end(), [](const auto& e){std::cout << e << ' ';});
-  std::cout << std::endl;
+  std::copy(names.begin(), names.end(), std::ostream_iterator<std::string>{std::cout, " "});
+  std::cout.put('\n');
 
   std::sort(names.begin(), names.end(), [](const auto& lhs, const auto& rhs){
     return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
@@ -30,7 +24,7 @@ int main(int argc, char** argv)
   // 訳: lexicographical_compareで辞書順比較してますが、元々std::stringは辞書順にソートされます。
 
   std::cout << "after:\n";
-  std::for_each(names.begin(), names.end(), [](const auto& e){std::cout << e << ' ';});
-  std::cout << std::endl;
+  std::copy(names.begin(), names.end(), std::ostream_iterator<std::string>{std::cout, " "});
+  std::cout.put('\n');
   return 0;
 }
