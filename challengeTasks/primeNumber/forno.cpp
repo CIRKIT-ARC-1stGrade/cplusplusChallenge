ad0ab437d84f5288fc867e5a2466230538be39d0
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-27 14:21:55 +0900

    Add comment for prime number

--- challengeTasks/primeNumber/forno.cpp
+++ challengeTasks/primeNumber/forno.cpp
@@ -17,8 +17,8 @@ int main(int argc, char** argv)
   if (limit <= 1) return 0;
   const std::uintmax_t search_limit {static_cast<std::uintmax_t>(std::ceil(std::sqrt(static_cast<long double>(limit))))};
 
-  std::valarray<bool> prims(true, limit + 1);
-  prims[std::slice(0, 2, 1)] = false;
+  std::valarray<bool> prims(true, limit + 1); // array of [0] to [limit]
+  prims[std::slice(0, 2, 1)] = false; // 0 and 1 aren't primes.
 
   for (std::uintmax_t i {2}; i < search_limit; ++i)
     if (prims[i])
