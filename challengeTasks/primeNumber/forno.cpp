a06d4b29b1e48e443c43a3032994fd2242b0ab6f
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-27 15:34:39 +0900

    Fix bug

--- challengeTasks/primeNumber/forno.cpp
+++ challengeTasks/primeNumber/forno.cpp
@@ -9,17 +9,17 @@ int main(int argc, char** argv)
 
   if (argc == 1) {
     std::cout << "Usage: " << *argv << " [number]\n" <<
-                 "[number] is max number of display numbers" << std::endl;
+                 "[number] is a positive number." << std::endl;
     return 1;
   }
 
   const std::uintmax_t limit {std::stoull(argv[1])}; // get search limit.
   if (limit <= 1) return 0;
-  const std::uintmax_t search_limit {static_cast<std::uintmax_t>(std::ceil(std::sqrt(static_cast<long double>(limit))))};
 
   std::valarray<bool> prims(true, limit + 1); // array of [0] to [limit]
-  prims[std::slice(0, 2, 1)] = false; // 0 and 1 aren't primes.
+  prims[std::slice(0, 2, 1)] = false; // 0 and 1 aren't primes. (not needed)
 
+  const std::uintmax_t search_limit {static_cast<std::uintmax_t>(std::floor(std::sqrt(static_cast<long double>(limit)))) + 1};
   for (std::uintmax_t i {2}; i < search_limit; ++i)
     if (prims[i])
       prims[std::slice(i*i, limit / i - (i - 1), i)] = false;
