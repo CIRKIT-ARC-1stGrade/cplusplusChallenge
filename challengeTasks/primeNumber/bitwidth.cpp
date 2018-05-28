6e85b2963287babd2b9ce91c5e26a067760c4891
Doi Yusuke <doi.yusuke806@mail.kyutech.jp>
2018-05-28 14:23:50 +0900

    Fix calc limit and use bitset

--- challengeTasks/primeNumber/bitwidth.cpp
+++ challengeTasks/primeNumber/bitwidth.cpp
@@ -1,3 +1,4 @@
+#include <bitset>
 #include <chrono>
 #include <cmath>
 #include <cstdint>
@@ -7,30 +8,30 @@
 using namespace std;
 
 template<typename T>
-constexpr auto check(const vector<uint32_t>& pflag, const T& n)
+constexpr auto check(const vector<bitset<32>>& pflag, const T& n)
 {
-  return pflag[n >> 6] & (1 << ((n >> 1) & 0x1f));
+  return pflag[n >> 6].test((n >> 1) & 0x1f);
 }
 
 template<typename T>
-constexpr void set(vector<uint32_t>& pflag, const T& n)
+constexpr void set(vector<bitset<32>>& pflag, const T& n)
 {
-  pflag[n >> 6] |= (1 << ((n >> 1) & 0x1f));
+  pflag[n >> 6].set((n >> 1) & 0x1f);
 }
 
 vector<uint64_t> sieve(uint64_t n) {
-  vector<uint32_t> pflag((n >> 6) + 1);
+  vector<bitset<32>> pflag(((n + 1) >> 6) + 1);
 
   uint64_t lmt = sqrt(n);
   for (uint64_t i = 3; i <= lmt; i += 2)
     if (!check(pflag, i))
-      for (uint64_t j = i * i, k = i << 1; j < n; j += k)
+      for (uint64_t j = i * i, k = i << 1; j <= n; j += k)
         set(pflag, j);
 
   vector<uint64_t> primes {};
   primes.reserve(50847534L);
   primes.emplace_back(2);
-  for (uint64_t i = 3; i < n; i += 2)
+  for (uint64_t i = 3; i <= n; i += 2)
     if (!check(pflag, i))
       primes.emplace_back(i);
 
